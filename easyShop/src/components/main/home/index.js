ac76ef8714ed324a4ebbf81ea3352bf17ddc8a11
zhangxin0723 <zx13835973692@163.com>
2019-07-25 16:51:05 +0800

    首页头部

--- easyShop/src/components/main/home/index.js
+++ easyShop/src/components/main/home/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import './index.scss'
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
+            <div className="home">
+                <div className="home_banner">
+
+                </div>
                 首页
             </div>
         )
