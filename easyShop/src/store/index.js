ed6903aa0d311e85f36657fd648afa41448956d6
zhangxin0723 <zx13835973692@163.com>
2019-07-25 12:00:15 +0800

    rem计算

---
+++ easyShop/src/store/index.js
@@ -0,0 +1,10 @@
+//引入模块
+import Home from './moduls/home'
+import Count from './setcount/count'
+//实例化模块
+const home = new Home()
+const count = new Count()
+export default {
+    home,
+    count
+}
