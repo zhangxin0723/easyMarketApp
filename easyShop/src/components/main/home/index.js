555635a9065996d8fd10d2e7e53442a221289c76
zhangxin0723 <zx13835973692@163.com>
2019-07-26 16:23:14 +0800

    首页渲染完毕

--- easyShop/src/components/main/home/index.js
+++ easyShop/src/components/main/home/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { inject, observer } from 'mobx-react'
 import './index.scss'
 import 'swiper/dist/css/swiper.css'
@@ -19,10 +19,12 @@ class Home extends Component {
                 el: this.refs.swiperPage,
             },
         })
-        new Swiper(this.refs.swiperTop,{
-            observer:true,
-            loop:true,
-            spaceBetween: 30
+        new Swiper(this.refs.swiperTop, {
+            observer: true,
+            spaceBetween: 15,
+            slidesPerView: 'auto',//开启slide宽度自定义功能  
+            centeredSlides: true,//首个居中  
+            loop: true,
         })
     }
     render() {
@@ -108,6 +110,11 @@ class Home extends Component {
                                     this.props.home.homeData && this.props.home.homeData.topicList.map(item => {
                                         return (<div className='swiper-slide' key={item.id}>
                                             <img src={item.item_pic_url} />
+                                            <div className='topGoodSubTitle'>
+                                                关爱他成长的每一个足迹
+                                                <span className='topGoodPrice'>￥0元起</span>
+                                            </div>
+                                            <div className='topGoodTitle'>专业运动品牌同厂，毛毛虫鞋买二送一</div>
                                         </div>)
                                     })
                                 }
@@ -115,7 +122,34 @@ class Home extends Component {
                         </div>
                     </div>
                 </div>
-                首页
+                <div className='cateGoryBox'>
+                    {
+                        this.props.home.homeData && this.props.home.homeData.categoryList.map(item => {
+                            return (<Fragment key={item.id}><div className='cateGoryName'>{item.name}</div>
+                                <div className='cateGoryGoodsWrap' >
+                                {
+                                    item.goodsList.map(val => {
+                                        return (
+                                            <a tag='div' href='#' key={val.id}>
+                                                <div className='goodsItemImg'>
+                                                    <img src={val.list_pic_url} />
+                                                </div>
+                                                <div className='goodsItemName'>{val.name}</div>
+                                                <div className='goodsItemPrice'>￥{val.retail_price}</div>
+                                            </a>
+                                        )
+                                    })
+                                }
+                                <a className='categoryMoreGoods'>
+                                    <div>更多{item.name}好物</div>
+                                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEMAAABDCAMAAADwFEhBAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAABLUExURf///4uLi7KysszMzHJycvb29qurq2ZmZv7+/m1tbZSUlN3d3Xd3d35+fsbGxtnZ2e/v7/z8/J2dneHh4evr676+voaGhri4uNPT04zm/X4AAAIzSURBVFjDvVjRlqsgDKwojgEsYq36/196pbZdRVyjh728eCxlIGEySbzd/nI0uWh7QwCZvhV5cxrgUdYIRl0+TgDIwQOQErlu5PTa6Fwo8jCDZCKIAjCldeufnS0NUAgGSnWfELLRxebcmE0o9+oAopv+ldn9eevnu18hcjrax5+T8l/mS0Ad2isVUO7t41rQs2K47EloXRxCgUbe1Y0EFQOpWhSayyBdoI2cuARpPg01odzeCMieCQZLCG+nIzzPRdQTtOZJlUFV5zAqhWy15I5Cng1taXBfvharV+bIVxuL4FjMkUEsj6GvKJ1eHGRAdk0uMwwfD9cYr2GMqKvPkUx1DcOZjxPKCG3D0ddRXf8urWEZlpuYelnUcx4BuWNCTSCR1OAIzUwVxWFlhiICoubIEwx3vCVqa7OYaaaYPPdSOW75/jKi55J0UrqNomv0/mHATcde94MzNzD+QeCHvUAgVRKFfwCn1GsRq6/VtMXA8VgmKDdjrG3hYGxt4ft0tmVYafns0/6EAG18+r5bxdbSyN2+Ocbk+kzUPM51XsztcP0dc6zY34m5KfY7tgbtxP5Hg1haKPuoBn2XTmni0Jg+qoXum5g4uSGuyT+5IUWO8rnSXoGwy6SdIGcnqR1S1DBXaqlbWEulqOmS1JZJatxXrc1u2B7xWnsnoe4Zotx+78Fy527vkaQHStKLpekJk/SmPz2yDntkze6Rg169k87J7nSvnuabQfDtorj47eK/j3/n9xya7EBtgAAAAABJRU5ErkJggg==" alt="more" />
+                                </a>
+                                </div>
+                            </Fragment>)
+                })
+            }
+
+                </div>
             </div>
         )
     }
