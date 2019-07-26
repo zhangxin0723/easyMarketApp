47cf5b5edd19ce9a73643e274fe7a29e8bbd425c
AL <l641152742@163.com>
2019-07-26 15:00:02 +0800

    专题

--- easyShop/src/components/main/Topic/index.js
+++ easyShop/src/components/main/Topic/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
-
+import './topic.scss'
 export default class Topic extends Component {
     render() {
         return (
-            <div>
-                主题
+            <div className='topic_wrap'>
+                {/* <div className='topic_cont'>
+                    <img src="https://yanxuan.nosdn.127.net/14943267735961674.jpg" alt=""/>
+                    <div className='topice_title'>关爱他成长的每一个足迹</div>
+                    <div className='topice_center'>专业运动品牌同厂，毛毛虫鞋买二送一</div>
+                    <div className='topic_price'>
+                        '0''元起'
+                    </div>
+                </div> */}
             </div>
         )
     }
