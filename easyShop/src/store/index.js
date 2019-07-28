a12aba51a34f0315bf34b8a79b4b1bfc537f7a47
zhangxin0723 <zx13835973692@163.com>
2019-07-28 16:43:18 +0800

    更新本地代码

--- easyShop/src/store/index.js
+++ easyShop/src/store/index.js
@@ -5,7 +5,7 @@ import Goods from './moduls/goods'
 //实例化模块
 const home = new Home()
 const login = new MyLogin()
-const goods =new Goods()
+const goods = new Goods()
 export default {
     home,
     login,
