0cff7bd5e624b43a974542d853d7f84c401b4d93
AL <l641152742@163.com>
2019-07-29 14:41:46 +0800

    购物车排版

--- easyShop/src/components/main/Cart/index.js
+++ easyShop/src/components/main/Cart/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import './index.scss'
 class Cart extends Component {
     render() {
         return (
@@ -20,7 +20,34 @@ class Cart extends Component {
                         </li>
                     </ul>
                 </header>
-                <section className="main"></section>
+                <section className="main">
+                    <div className='main_box'>
+                        <div className='main_checkout'>
+                            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAMAAACf4xmcAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAABCUExURUdwTMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzAV+Z0EAAAAVdFJOUwAJ+KUEFTPay2bzRXdZ7RkhmJ6qJOWhY+QAAAEDSURBVDjLnZTplsIgDIUNWwK2tdt9/1cdxHGmVcAc+dH25Hw0+71cvjhztDIZM4mNc4txo+BwZKxSVwbSFoMn8iFuCeDrG0RLNkc6GGK+ttCZ8gIzuJcgBgPxJ4rB4T2OkM0HjgRyq8V7Y8i/3/V06YVb/nKECa0qBYPffB1jaFd8AD8+RrBrY8R41FkQew2MkPtrR6IeRglzoW1/HrbizfZ9Pv8jCH0slOAm+D7mMeUn4PoYwegxpVNlCsqCKMurbJay9R8GyT0HSTmWeciTYsh7K+MPK1MW0H9eQOU652sqcch+15rUrFQXLpuFy7ksXLYuXDUZbBZ9v4sqiqju34jyD97JD4dkfgo1AAAAAElFTkSuQmCC" alt=""/>
+                        </div>
+                        <div className='main_goods_img'>
+                            <img src="http://yanxuan.nosdn.127.net/66b9f1638c0517d179262f14ed1345f9.png" alt=""/>
+                        </div>
+                        <div className="main_cart_cont">
+                            <div>绿豆糕 80克（4枚入）</div>
+                            <div></div>
+                            <div style={{color:'red'}}>￥12.9</div>
+                        </div>
+                        <div className='main_cart_num'>
+                            x4
+                        </div>
+                    </div>
+                    <div className='footer'>
+                        <div className='footer_checkout'>
+                            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAMAAACf4xmcAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAABCUExURUdwTMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzAV+Z0EAAAAVdFJOUwAJ+KUEFTPay2bzRXdZ7RkhmJ6qJOWhY+QAAAEDSURBVDjLnZTplsIgDIUNWwK2tdt9/1cdxHGmVcAc+dH25Hw0+71cvjhztDIZM4mNc4txo+BwZKxSVwbSFoMn8iFuCeDrG0RLNkc6GGK+ttCZ8gIzuJcgBgPxJ4rB4T2OkM0HjgRyq8V7Y8i/3/V06YVb/nKECa0qBYPffB1jaFd8AD8+RrBrY8R41FkQew2MkPtrR6IeRglzoW1/HrbizfZ9Pv8jCH0slOAm+D7mMeUn4PoYwegxpVNlCsqCKMurbJay9R8GyT0HSTmWeciTYsh7K+MPK1MW0H9eQOU652sqcch+15rUrFQXLpuFy7ksXLYuXDUZbBZ9v4sqiqju34jyD97JD4dkfgo1AAAAAElFTkSuQmCC" alt=""/>
+                        </div>
+                        <div className='footer_checkall'>
+                            已选(6)  ￥1539
+                        </div>
+                        <div className='footer_compile'>编辑</div>
+                        <div className='footer_order'>下单</div>
+                    </div>
+                </section>
             </div>
         )
     }
