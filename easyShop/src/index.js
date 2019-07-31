350c5f5f8557baa0275a2bd9401c33fe2bd6a023
zhangxin0723 <zx13835973692@163.com>
2019-07-31 09:18:26 +0800

    修改边框

--- easyShop/src/index.js
+++ easyShop/src/index.js
@@ -6,12 +6,9 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import { Provider } from 'mobx-react'
 import store from './store/index'
 import 'antd-mobile/dist/antd-mobile.css'
-
-import Login from './components/login/login'
 //路由
 import routes from './router/config'
-//判断是否登录
-import { getToken } from './utils/index'
+
 //解决300ms延迟问题
 var FastClick = require('fastclick');
 FastClick.attach(document.body);
